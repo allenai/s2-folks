63876cce824ea59027f22d7f4759f12b973db594
Jason Dunkelberger <dirkraft@gmail.com>
2023-05-10 15:11:15 -0700

    Simple test discovery and runner inside github action

---
+++ examples/python/run_tests.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Run by CI GithubAction to test examples, for those that provide tests.
+
+cd "$(dirname "$(readlink -f "$0")")"
+
+for example_test in */test.sh ; do
+  echo "Running $example_test"
+  example_dir="$(dirname "$example_test")"
+  pushd . > /dev/null
+  cd "$example_dir"
+
+  if [ -f "requirements.txt" ] ; then
+    # Yes, this means dependencies from different examples will mash on top of each other.
+    # Solve it later, if it becomes a problem.
+    echo "  Installing dependencies"
+    pip install -r "requirements.txt"
+  fi
+
+  ./test.sh
+
+  if [ $? -eq 0 ]; then
+    echo "  $example_test succeeded!"
+  else
+    echo "  $example_test exited non-zero and may have errored. Please investigate."
+  fi
+
+  popd > /dev/null
+done

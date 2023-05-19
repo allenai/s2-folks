0f59937ea457364364f298e1d29cd7bcc783fad4
Jason <jasond+dirkraft@allenai.org>
2023-05-19 13:14:11 -0700

    improvements to running examples with tests (#94)
    Some symbols to make it easier to scan test outputs
    > before test script
    >> after test script
    List failed test scripts at the end.
    Look specifically for test.py instead of an arbitrary test script with a shebang

--- examples/python/run_tests.sh
+++ examples/python/run_tests.sh
@@ -3,8 +3,8 @@
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-for example_test in */test ; do
-  echo "Running $example_test"
+for example_test in */test.py ; do
+  echo "> Running $example_test"
   example_dir="$(dirname "$example_test")"
   pushd . > /dev/null
   cd "$example_dir"
@@ -12,22 +12,23 @@ for example_test in */test ; do
   if [ -f "requirements.txt" ] ; then
     # Yes, this means dependencies from different examples will mash on top of each other.
     # Solve it later, if it becomes a problem.
-    echo "  Installing dependencies"
+    echo ">> Installing dependencies"
     pip install -r "requirements.txt"
   fi
 
-  ./test
+  python ./test.py
 
   if [ $? -eq 0 ]; then
-    echo "  $example_test succeeded!"
+    echo ">> $example_test succeeded!"
   else
-    tests_failed=yes
-    echo "  $example_test exited non-zero and may have errored. Please investigate."
+    echo ">> $example_test exited non-zero and may have errored."
+    tests_failed="$tests_failed"$'\n'"• $example_test"
   fi
 
   popd > /dev/null
 done
 
 if [ "$tests_failed" != "" ] ; then
+  echo "These test scripts failed: $tests_failed"
   exit 1
 fi

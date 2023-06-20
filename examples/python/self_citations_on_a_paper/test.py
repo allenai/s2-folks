74a36a71261e29775a3d6afa9bae14bedc48310f
Jason Dunkelberger <dirkraft@gmail.com>
2023-06-20 09:58:37 -0700

    Fix self_citations_on_a_paper/test.py
    though if this fails again this may be too brittle, subject to underlying data changes

--- examples/python/self_citations_on_a_paper/test.py
+++ examples/python/self_citations_on_a_paper/test.py
@@ -1,7 +1,8 @@
+import re
 import subprocess
 
 output = subprocess.getoutput('python main.py CorpusID:3658586')
 print(output)
 # This author has built on their own work frequently so should be a stable test.
-assert 'T. Wenzl in citing paper' in output
-assert 'T. Wenzl in referenced paper' in output
+assert re.search('Vandenplas.*in citing paper', output)
+assert re.search('Vandenplas.*in referenced paper', output)

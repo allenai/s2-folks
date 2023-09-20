bd8543d1b194726a1e4576fd48f541819b4c8b8f
Miles <miles@milescrawford.com>
2023-09-20 16:42:30 -0700

    format

--- examples/python/search_bulk/get_sturgeon_dataset.py
+++ examples/python/search_bulk/get_sturgeon_dataset.py
@@ -2,9 +2,9 @@ import requests
 import json
 
 query = "sturgeon"
-fields="title,year"
+fields = "title,year"
 
-url = f'http://api-dev.semanticscholar.org/graph/v1/paper/search/bulk?query={query}&fields={fields}'
+url = f"http://api-dev.semanticscholar.org/graph/v1/paper/search/bulk?query={query}&fields={fields}"
 r = requests.get(url).json()
 
 print(f"Will retrieve {r['total']} documents")
@@ -12,13 +12,13 @@ retrieved = 0
 
 with open(f"{query}.jsonl", "a") as file:
     while True:
-        for paper in r['data']:
+        for paper in r["data"]:
             print(json.dumps(paper), file=file)
-        token = r['token']
+        token = r["token"]
         if token:
-            retrieved += len(r['data'])
+            retrieved += len(r["data"])
             print(f"Retrieved {retrieved} papers...")
         else:
             break
-        r = requests.get(f'{url}&token={token}').json()
+        r = requests.get(f"{url}&token={token}").json()
 print("Done!")

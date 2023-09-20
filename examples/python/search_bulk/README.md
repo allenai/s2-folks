6fe026a5115d5c5bcb2fe78722b91dfd5b9cb956
Miles <miles@milescrawford.com>
2023-09-20 16:41:29 -0700

    example of using bulk search

---
+++ examples/python/search_bulk/README.md
@@ -0,0 +1,9 @@
+S2 APIs involved:
+* [GET /graph/v1/paper/search/bulk](https://api.semanticscholar.org/api-docs/graph#tag/Paper-Data/operation/get_graph_get_paper_bulk)
+
+This example fetches a subset of paper data specified by a simple query and writes a JSON-lines archive of the results.
+
+To run:
+```
+python get_sturgeon_dataset.py
+```
---
+++ examples/python/search_bulk/get_sturgeon_dataset.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+query = "sturgeon"
+fields="title,year"
+
+url = f'http://api-dev.semanticscholar.org/graph/v1/paper/search/bulk?query={query}&fields={fields}'
+r = requests.get(url).json()
+
+print(f"Will retrieve {r['total']} documents")
+retrieved = 0
+
+with open(f"{query}.jsonl", "a") as file:
+    while True:
+        for paper in r['data']:
+            print(json.dumps(paper), file=file)
+        token = r['token']
+        if token:
+            retrieved += len(r['data'])
+            print(f"Retrieved {retrieved} papers...")
+        else:
+            break
+        r = requests.get(f'{url}&token={token}').json()
+print("Done!")
